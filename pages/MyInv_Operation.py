00edd415bd26181863616c5f195a401b04ec65d1
Luis Rodrigo Lima <90912526+luisrodrigol@users.noreply.github.com>
2023-06-05 09:46:25 -0300

    Update MyInv_Operation.py

--- pages/MyInv_Operation.py
+++ pages/MyInv_Operation.py
@@ -351,7 +351,7 @@ def main():
             st_profile_report(pr)
             
         except Exception as e:
-                st.warning("Inventário vazio!", icon="⚠️")
+                st.warning("Inventário vazio ou sem valores para comparar!", icon="⚠️")
                 pass
         
 
